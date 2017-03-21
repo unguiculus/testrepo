bdf3bcc7fd22b89fd6e4bc9a76296d3e07294051
Reinhard Nägele <reinhard.naegele@codecentric.de>
2017-03-21 08:50:46 +0100

    test

---
+++ core/src/test/java/io/github/unguiculus/test/core/TestTest.java
@@ -0,0 +1,16 @@
+package io.github.unguiculus.test.core;
+
+import org.junit.Test;
+
+public class TestTest {
+
+    @Test
+    public void test1() throws Exception {
+        throw new Exception("failed");
+    }
+
+    @Test
+    public void test2() throws Exception {
+        //
+    }
+}
